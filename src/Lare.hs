91b9dac65a5263f650819ea03b83915c04df7b55
mschaper <michael.schaper@uibk.ac.at>
2018-01-26 15:30:57 +0100

    working example

--- src/Lare.hs
+++ src/Lare.hs
@@ -1,8 +1,14 @@
-{-# LANGUAGE RecordWildCards, TypeOperators #-}
+{-# LANGUAGE RecordWildCards, TypeOperators, FlexibleInstances #-}
 module Lare where
 
 import Data.List ((\\), nub)
 import Data.Function ((&))
+import Data.Monoid ((<>))
+
+import Text.PrettyPrint.ANSI.Leijen (Pretty, pretty)
+import qualified Text.PrettyPrint.ANSI.Leijen as PP
+
+import Debug.Trace
 
 
 --- * Cfg ------------------------------------------------------------------------------------------------------------
@@ -28,10 +34,10 @@ nodes :: Eq v => Cfg v e -> [v]
 nodes cfg = nub $ fmap src cfg ++ fmap dst cfg
 
 sources :: Eq v => Cfg v e ->  [v]
-sources cfg = fmap src cfg \\ fmap dst cfg
+sources cfg = nub (fmap src cfg) \\ nub (fmap dst cfg)
 
 sinks :: Eq v => Cfg v e -> [v]
-sinks cfg = fmap dst cfg \\ fmap src cfg
+sinks cfg = nub (fmap dst cfg) \\ nub (fmap src cfg)
 
 data V v = V v | Out v | In v
   deriving (Eq, Ord, Show)
@@ -45,6 +51,7 @@ data Top v e = Top (Cfg (V v) e) [Tree v e]
 
 data Tree v e = Tree
   { program     :: Cfg (V v) e
+  , loopid      :: Int
   , outer       :: [v]
   , inner       :: [v]
   , cuts        :: [v]
@@ -68,21 +75,17 @@ class Att e where
   compose   :: e -> e -> e
   alternate :: e -> e -> e
   closure   :: e -> e
-  loop      :: e -> e
+  loop      :: Int -> e -> e
 
 compose', alternate' :: Att e => Edge v e -> Edge v e -> Edge v e
 compose'   = lift2 compose
-alternate'   = lift2 alternate
+alternate' = lift2 alternate
 
 closure' :: Att e => Edge v e -> Edge v e
 closure' = lift1 closure
 
--- loop' :: Att e => Edge v e -> Edge v e -> Edge v e
--- loop'    = lift2 loop
-
-loop' :: Att e => Edge v e -> Edge v e
-loop'    = lift1 loop
-
+loop' :: Att e => Int ->  Edge v e -> Edge v e
+loop' i    = lift1 $ loop i
 
 --- * Rip ------------------------------------------------------------------------------------------------------------
 
@@ -98,8 +101,8 @@ ripOne v cfg =
   (vvs, uvs, vws, uws)
     = partition3
       (\e -> src e == v && dst e == v)
-      (\e -> src e == v)
-      (\e -> dst e == v) cfg
+      (\e -> dst e == v)
+      (\e -> src e == v) cfg
   in
   mergeParallel alternate' $ uws ++ chainAll (combinator vvs) uvs vws
 
@@ -121,35 +124,55 @@ ripOne v cfg =
 rip :: (Eq v, Att e) => [v] -> T v e
 rip vs cfg = foldr ripOne cfg vs
 
-ripWith :: (Eq v, Att e) => [v] -> T v e
-ripWith vs cfg = loop' <$> rip vs cfg
+ripWith :: (Eq v, Att e) => Int -> [v] -> T v e
+ripWith i vs cfg = loop' i <$> rip vs cfg
 
 
-convert :: (Eq v, Att e) => Top v e -> Cfg (V v) e
+convert :: (Eq v, Att e, Show v, Show e, Pretty e) => Top v e -> Cfg (V v) e
 convert (Top p ps) =
   p
     & mappend (concatMap convert' ps)
+    & debug "T:append subtrees"
     & mappend [ edge (V v) epsilon (In v)  | v <- inner ]
     & mappend [ edge (Out v) epsilon (V v) | v <- inner ]
+    & debug "T:add inner ports subtrees"
+    & debug "T:rip inner ports"
     & rip [In v  | v <- inner ]
     & rip [Out v | v <- inner ]
+    & debug "T:rip inner ports"
+    & \g -> traceShow (nodes g \\ (sources g ++ sinks g)) g
+    & \g -> traceShow (nodes g) g
+    & \g -> traceShow (sources g) g
+    & \g -> traceShow (sinks g) g
+    & \g -> rip (nodes g \\ (sources g ++ sinks g)) g
+    & debug "result"
   where inner = concatMap outer ps
 
-convert' :: (Eq v, Att e) => Tree v e -> Cfg (V v) e
+convert' :: (Eq v, Att e, Show v, Show e, Pretty e) => Tree v e -> Cfg (V v) e
 convert' Tree{..}
   | null program = []
-  | otherwise    =
-    program
-      & mappend (concatMap convert' subprograms)
-      & mappend [ edge (V v) epsilon (In v)  | v <- inner ]
-      & mappend [ edge (Out v) epsilon (V v) | v <- inner ]
-      & rip [In v  | v <- inner ]
-      & rip [Out v | v <- inner ]
-      & mappend [ edge (In v) epsilon (V v)  | v <- outer ]
-      & mappend [ edge (V v) epsilon (Out v) | v <- outer ]
-      & \g -> ripWith (nodes g \\ ([ In v | v <- outer ] ++ [ Out v | v <- outer ])) g
+  | otherwise    = trace ("> tree\n" ++ ppcfg p ++ "<\n") p
+    where 
+    p = 
+      program
+        & debug "cfg"
+        & mappend (concatMap convert' subprograms)
+        & debug "add subprogram"
+        & mappend [ edge (V v) epsilon (In v)  | v <- inner ]
+        & mappend [ edge (Out v) epsilon (V v) | v <- inner ]
+        & debug "add inner ports"
+        & rip [In v  | v <- inner ]
+        & rip [Out v | v <- inner ]
+        & debug "rip inner nodes"
+        & mappend [ edge (In v) epsilon (V v)  | v <- outer ]
+        & mappend [ edge (V v) epsilon (Out v) | v <- outer ]
+        & debug "add outer ports"
+        & \g -> traceShow (nodes g \\ (sources g ++ sinks g)) g
+        -- & \g -> loop foldr (\v g' -> trace ("rip\n" ++ ppcfg g') ripOne v g') g (nodes g \\ (sources g ++ sinks g))
+        & \g -> ripWith loopid (nodes g \\ (sources g ++ sinks g)) g
+        & debug "rip internal nodes"
 
-
+debug s cfg = trace ("[" ++ s ++ "]\n" ++ ppcfg cfg) cfg
 
 
 --- * Util -----------------------------------------------------------------------------------------------------------
@@ -166,42 +189,69 @@ partition3 p1 p2 p3 = foldr select ([],[],[],[]) where
 
 --- *  RE ------------------------------------------------------------------------------------------------------------
 
-data RE v a 
+data RE a
   = Sym a 
   | Epsilon 
-  | Concatenate (RE v a) (RE v a) 
-  | Alternate (RE v a) (RE v a) 
-  | Star (RE v a) 
-  | Loop (RE v a)
+  | Concatenate (RE a) (RE a) 
+  | Alternate (RE a) (RE a) 
+  | Star (RE a) 
+  | Loop Int (RE a)
   deriving Show
 
-instance Att (RE v a) where
+instance Pretty (RE Char) where
+  pretty (Sym c)                 = PP.char c
+  pretty Epsilon                 = PP.char '_'
+  pretty (Concatenate Epsilon a) = pretty a
+  pretty (Concatenate a Epsilon) = pretty a
+  pretty (Concatenate a b)       = pretty a <> pretty b
+  pretty (Alternate a b)         = PP.parens $ pretty a <> PP.char '|' <> pretty b
+  pretty (Star (Sym c))          = PP.char c <> PP.char '*'
+  pretty (Star a)                = PP.parens (PP.pretty a) <> PP.char '*'
+  pretty (Loop i a)              = PP.brackets $ PP.int i <> PP.space <> PP.pretty a
+
+
+-- instance Show a => Show (RE a) where
+--   show (Sym a)                 = show a
+--   show Epsilon                 = "_"
+--   show (Concatenate Epsilon b) = show b
+--   show (Concatenate a Epsilon) = show a
+--   show (Concatenate a b)       = show a ++ show b
+--   show (Alternate Epsilon b)   = "(_|" ++ show b ++ ")"
+--   show (Alternate a Epsilon)   = "(" ++ show a ++ "|_)"
+--   show (Alternate a b)         = "(" ++ show a ++ "|" ++ show b ++ ")"
+--   show (Star a)                = "(" ++ show a ++ show ")*"
+--   show (Loop i a)              = "["++ show i ++ " " ++ show a ++ "]"
+
+
+instance Att (RE v) where
   epsilon   = Epsilon
   compose   = Concatenate
   alternate = Alternate
   closure   = Star
   loop      = Loop
 
-ppcfg :: (Show v, Show e) => Cfg v e -> String
-ppcfg = unlines . map k 
-  where k e = show (src e) ++ "\t~>\t" ++ show (dst e) ++ "\t\t" ++ show (att e)
 
-ex :: Top String (RE String String)
-ex = 
+
+ppcfg :: (Pretty e, Show v, Show e) => Cfg v e -> String
+ppcfg = unlines . map k 
+  where k e = show (src e) ++ "\t~>\t" ++ show (dst e) ++ "\t\t" ++ (show $ pretty (att e))
+
+ex1 :: Top Char (RE Char)
+ex1 = 
   Top 
     [a,b,e]
     [ Tree 
       { program     = [c,d]
-      , outer       = ["B","C"]
+      , loopid      = 1
+      , outer       = ['C','B']
       , inner       = []
-      , cuts        = ["c"]
+      , cuts        = ['c']
       , subprograms = [] }
     ]
   where
-    a = edge (V "I") Epsilon        (V "A")
-    b = edge (V "A") (Sym "copy12") (V "B")
-    c = edge (V "B") (Sym "E")      (V "C")
-    d = edge (V "C") (Sym "sum352") (V "B")
-    e = edge (V "C") (Sym "copy52") (V "F")
-
+    a = edge (V 'I') (Sym 'a') (V 'A')
+    b = edge (V 'A') (Sym 'b') (V 'B')
+    d = edge (V 'C') (Sym 'd') (V 'B')
+    c = edge (V 'B') (Sym 'c') (V 'C')
+    e = edge (V 'C') (Sym 'e') (V 'F')
 
