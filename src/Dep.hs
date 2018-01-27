4f2965329e29663714a94ee8c08406928a48cb4a
Michael <michael.schaper@uibk.ac.at>
2018-01-27 19:56:50 +0100

    add  domain

--- src/Dep.hs
+++ src/Dep.hs
@@ -1,16 +1,43 @@
--- {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TypeFamilies #-}
 module Dep where
 
-import           Data.Set (Set)
-import qualified Data.Set as S
-import Control.Monad.Reader
+import           Data.Set             (Set)
+import qualified Data.Set             as S
+
+import           Prelude              hiding (iterate)
 
 -- FIXME: when do we need all variables; can we just use somethin like an special id flow?
+
+
+data Dom st e = Dom
+  { ctx         :: st
+  , identity    :: st -> e
+  , concatenate :: st -> e -> e -> e
+  , alternate   :: st -> e -> e -> e
+  , closure     :: st -> Maybe (Annot e) -> e -> e
+  , iterate     :: st -> Maybe (Annot e) -> e -> e }
+
+
+type family Annot a :: *
+type instance Annot (F v) = v
+
+type Flow v = Dom [v] (F v)
+
+flow :: Ord v => [v] -> Flow v
+flow vs = Dom
+  { ctx         = vs
+  , identity    = identity'
+  , concatenate = const concatenate'
+  , alternate   = const alternate'
+  , closure     = closure'
+  , iterate     = iterate' }
+
 
 --- * Dependency -----------------------------------------------------------------------------------------------------
 
 data E v =  v :> v deriving (Eq, Ord, Show)
-data D = Identity | Additive | Multiplicative | Exponential deriving (Eq, Ord, Show)
+data D = Identity | Additive | Multiplicative | Exponential 
+  deriving (Eq, Ord, Show, Enum)
 
 type U v = (D, E v)
 type B v = (E v, E v)
@@ -20,22 +47,22 @@ data F v = F
   , binary :: Set (B v) }
   deriving Show
 
-type Flow v = Reader [v] (F v)
-
 
 empty :: F v
 empty = F { unary = S.empty, binary = S.empty}
 
 isSubsetOf :: Ord v => F v -> F v -> Bool
-isSubsetOf f1 f2 = unary f1 `S.isSubsetOf` unary f2 && binary f1 `S.isSubsetOf` binary f2
+isSubsetOf f1 f2 =
+  unary f1 `S.isSubsetOf` unary f2 &&
+  binary f1 `S.isSubsetOf` binary f2
 
 union :: Ord v => F v -> F v -> F v
-union f1 f2 = F 
-  { unary = unary f1 `S.union` unary f2 
+union f1 f2 = F
+  { unary = unary f1 `S.union` unary f2
   , binary = binary f1 `S.union` binary f2 }
 
-identity :: Reader [v] [U v]
-identity = ask >>= \vs -> return [ (Identity, v :> v) | v <- vs ]
+idep :: [v] -> [U v]
+idep vs = [ (Identity, v :> v) | v <- vs ]
 
 complete :: Ord v => [U v] -> F v
 complete ds = F
@@ -48,29 +75,32 @@ complete ds = F
        , i /= j || k /= l
        , d1 `max` d2 <= Additive ]
 
-skip :: Ord v => Flow v
-skip = complete <$> identity
+-- skip :: Ord v => F v
+-- skip =
 
-plus :: Ord v => v -> v -> v -> Flow v
-plus r s t = ask >>= \vs -> return $ complete $
-  let ids =  [ (Identity, v :> v) | v <- vs, v /= r ] in
-  if s /= t
-  then (Additive, s :> r): (Multiplicative, t :> r): ids
-  else (Multiplicative, s :> r) : ids
+-- plus :: Ord v => v -> v -> v -> Flow v
+-- plus r s t = ask >>= \vs -> return $ complete $
+--   let ids =  [ (Identity, v :> v) | v <- vs, v /= r ] in
+--   if s /= t
+--   then (Additive, s :> r): (Multiplicative, t :> r): ids
+--   else (Multiplicative, s :> r) : ids
 
-mult :: Ord v => v -> v -> v -> Flow v
-mult r s t = ask >>= \vs -> return $ complete $
-  (Multiplicative, s :> r) :(Multiplicative, t :> r) : [ (Identity, v :> v) | v <- vs, v /= r ]
+-- mult :: Ord v => v -> v -> v -> Flow v
+-- mult r s t = ask >>= \vs -> return $ complete $
+--   (Multiplicative, s :> r) :(Multiplicative, t :> r) : [ (Identity, v :> v) | v <- vs, v /= r ]
 
---- * Operations -----------------------------------------------------------------------------------------------------
+-- --- * Operations -----------------------------------------------------------------------------------------------------
 
-alternate :: Ord v => F v -> F v -> F v
-alternate f1 f2 = F
+identity' :: Ord v => [v] -> F v
+identity' = complete . idep
+
+alternate' :: Ord v => F v -> F v -> F v
+alternate' f1 f2 = F
   { unary  = unary f1  `S.union` unary f2
   , binary = binary f1 `S.union` binary f2}
 
-concatenate :: Ord v => F v -> F v -> F v
-concatenate f1 f2 = F
+concatenate' :: Ord v => F v -> F v -> F v
+concatenate' f1 f2 = F
   { unary =
       S.fromList $
         [ (d1 `max` d2   , i :> k) | (d1, i :> x) <- us1, (d2, z :> k) <- us2, x == z ] ++
@@ -87,13 +117,18 @@ concatenate f1 f2 = F
     bs1 = S.toList (binary f1)
     bs2 = S.toList (binary f2)
 
--- iterate ::  Ord v => v -> F v -> Flow v
--- iterate v f = lfp ==  
---   where
---   lfp f = ask >>= \vs -> go vs f empty f
---   go vs new old action
---     | new `isSubsetOf` old = old
---     | otherwise            = go vs new' new f
---       where new' = complete [ (Identity, v :> v) | v <- vs ] `union` old `union` (old `concatenate` action)
+closure' _ _ f = f
+
+correct' :: Ord v => v -> F v -> F v
+correct' v f = f `union` f { unary = unary' }
+  where unary' =  S.fromList [ (succ d, v :> j) | (d, j :> i) <- S.toList (unary f), j == i, d == Additive || d == Multiplicative ]
+
+iterate' _  Nothing f = error "oh no"
+iterate' vs (Just v) f = correct' v $ lfp f empty f
+  where
+  lfp new old action
+    | new `isSubsetOf` old = old
+    | otherwise            = lfp new' new f
+      where new' = complete [ (Identity, v :> v) | v <- vs ] `union` old `union` (old `concatenate'` action)
 
 
--- src/Lare.hs
+++ src/Lare.hs
@@ -1,6 +1,8 @@
-{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE RecordWildCards   #-}
+{-# LANGUAGE FlexibleContexts  #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE TypeOperators     #-}
+{-# LANGUAGE TypeFamilies     #-}
 {-# OPTIONS_GHC -fno-warn-name-shadowing -fno-warn-missing-signatures #-}
 module Lare where
 
@@ -12,6 +14,8 @@ import           Text.PrettyPrint.ANSI.Leijen (Pretty, pretty)
 import qualified Text.PrettyPrint.ANSI.Leijen as PP
 
 import           Debug.Trace
+
+import Dep (Dom(..), Annot)
 
 
 
@@ -50,64 +54,56 @@ data V v = V v | Out v | In v
 
 --- * Program --------------------------------------------------------------------------------------------------------
 
+
 data Top a b = Top a [Tree b]
   deriving Show
 
 data Tree a = Tree a [Tree a]
   deriving Show
 
+
+
+
+
+
+
+
+--- * Loop -----------------------------------------------------------------------------------------------------------
+
 data Loop v e = Loop
   { program :: Cfg (V v) e
-  , loopid  :: Int
+  , loopid  :: Annot e
   , outer   :: [v]
   , inner   :: [v]
   , cuts    :: [v] }
-  deriving Show
 
 type Program v e = Top (Cfg (V v) e) (Loop v e)
 
 
 
--- draw :: (Show e, Show v) => Top e v -> [String]
--- draw (Top p ps) = ("P: " ++ show p)  : drawSubTrees ps where
---   draw' t@Tree{} = ("p:" ++ show (program t) ++ " c: " ++ show (cuts t))  : drawSubTrees (subprograms t)
---   shift first other = zipWith (++) (first : repeat other)
---   drawSubTrees []     = []
---   drawSubTrees [t]    = "|" : shift "`- " "   " (draw' t)
---   drawSubTrees (t:ts) = "|" : shift "+- " "|  " (draw' t) ++ drawSubTrees ts
+
 
 
 --- * Attribute Domain -----------------------------------------------------------------------------------------------
-
-data Dom st e = Dom
-  { ctx    :: st
-  , identity   :: st -> e
-  , concatenate   :: st -> e -> e -> e
-  , alternate :: st -> e -> e -> e
-  , closure   :: st -> e -> e
-  , iterate   :: st -> Int -> e -> e }
 
 identity' :: Dom st e -> e
 identity' Dom{..} = identity ctx
 
 concatenate', alternate' :: Dom st e -> Edge v e -> Edge v e -> Edge v e
-concatenate'   Dom{..} = lift2 (concatenate ctx)
-alternate' Dom{..} = lift2 (alternate ctx)
+concatenate' Dom{..} = lift2 (concatenate ctx)
+alternate' Dom{..}   = lift2 (alternate ctx)
 
-closure' :: Dom st e -> Edge v e -> Edge v e
-closure' Dom{..} = lift1 (closure ctx)
-
-iterate' :: Dom st e -> Int -> Edge v e -> Edge v e
-iterate' Dom{..} = lift1 . iterate ctx
+closure', iterate' :: Dom st e -> Maybe (Annot e) -> Edge v e -> Edge v e
+closure' Dom{..} a e = lift1 (closure ctx a) e
+iterate' Dom{..} a e = lift1 (iterate ctx a) e
 
 -- --- * Rip ------------------------------------------------------------------------------------------------------------
 
 
 type T v e = Cfg v e -> Cfg v e
 
-
-ripOne :: Eq v => Dom st e -> v -> T v e
-ripOne dom v cfg =
+ripOne :: Eq v => Dom st e -> Maybe (Annot e) -> v -> T v e
+ripOne dom a v cfg =
   let
   (vvs, uvs, vws, uws)
     = partition3
@@ -122,7 +118,7 @@ ripOne dom v cfg =
   compose' = concatenate' dom
 
   combinator [] e1 e2 = e1 `compose'` e2
-  combinator es e1 e2 = e1 `compose'` (closure' dom) (foldr1 (alternate' dom) es) `compose'` e2
+  combinator es e1 e2 = e1 `compose'` (closure' dom a) (foldr1 (alternate' dom) es) `compose'` e2
 
   chainAll k es ds = [ k e1 e2 | e1 <- es, e2 <- ds ]
 
@@ -133,11 +129,12 @@ ripOne dom v cfg =
     | src e1 == src e2 && dst e1 == dst e2 = k e1 e2: es
     | otherwise                            = e2 : mergeOne k e1 es
 
-rip :: Eq v => Dom st e -> [v] -> T v e
-rip dom vs cfg = foldr (ripOne dom) cfg vs
+rip :: Eq v => Dom st e -> Maybe (Annot e) -> [v] -> T v e
+rip dom a vs cfg = foldr (ripOne dom a) cfg vs
 
-ripWith :: Eq v => Dom st e -> Int -> [v] -> T v e
-ripWith dom i vs cfg = iterate' dom i <$> (rip dom) vs cfg
+ripWith :: Eq v => Dom st e -> Maybe (Annot e) -> [v] -> T v e
+ripWith dom a vs cfg = iterate' dom a <$> (rip dom a) vs cfg
+
 
 
 convert dom (Top p ps) = Top p' ps'
@@ -151,14 +148,14 @@ convert dom (Top p ps) = Top p' ps'
     & mappend [ edge (Out v) (identity' dom) (V v) | v <- inner ]
     & debug "T:add inner ports subtrees"
     & debug "T:rip inner ports"
-    & rip dom [In v  | v <- inner ]
-    & rip dom [Out v | v <- inner ]
+    & rip dom Nothing [In v  | v <- inner ]
+    & rip dom Nothing [Out v | v <- inner ]
     & debug "T:rip inner ports"
     & \g -> traceShow (nodes g \\ (sources g ++ sinks g)) g
     & \g -> traceShow (nodes g) g
     & \g -> traceShow (sources g) g
     & \g -> traceShow (sinks g) g
-    & \g -> rip dom (nodes g \\ (sources g ++ sinks g)) g
+    & \g -> rip dom Nothing (nodes g \\ (sources g ++ sinks g)) g
     & debug "result"
     where inner = concatMap (\(Tree l _) -> outer l) ps
 
@@ -175,15 +172,15 @@ convert' dom (Tree Loop{..} ps) = Tree p' ps'
       & mappend [ edge (V v)   (identity' dom) (In v)  | v <- inner ]
       & mappend [ edge (Out v) (identity' dom) (V v) | v <- inner ]
       & debug "add inner ports"
-      & rip dom [In v  | v <- inner ]
-      & rip dom [Out v | v <- inner ]
+      & rip dom (Just loopid) [In v  | v <- inner ]
+      & rip dom (Just loopid) [Out v | v <- inner ]
       & debug "rip inner nodes"
       & mappend [ edge (In v) (identity' dom) (V v)  | v <- outer ]
       & mappend [ edge (V v) (identity' dom) (Out v) | v <- outer ]
       & debug "add outer ports"
       & \g -> traceShow (nodes g \\ (sources g ++ sinks g)) g
       -- & \g -> loop foldr (\v g' -> trace ("rip\n" ++ ppcfg g') ripOne v g') g (nodes g \\ (sources g ++ sinks g))
-      & \g -> ripWith dom loopid (nodes g \\ (sources g ++ sinks g)) g
+      & \g -> ripWith dom (Just loopid) (nodes g \\ (sources g ++ sinks g)) g
       & debug "rip internal nodes"
 
 debug s cfg = trace ("[" ++ s ++ "]\n" ++ ppcfg cfg) cfg
@@ -201,7 +198,9 @@ partition3 p1 p2 p3 = foldr select ([],[],[],[]) where
 
 
 
--- -- --- *  RE ------------------------------------------------------------------------------------------------------------
+--- *  RE ------------------------------------------------------------------------------------------------------------
+
+type instance Annot (RE a) = Int
 
 data RE a
   = Sym a
@@ -209,20 +208,8 @@ data RE a
   | Concatenate (RE a) (RE a)
   | Alternate (RE a) (RE a)
   | Star (RE a)
-  | Looper Int (RE a)
+  | Iterate Int (RE a)
   deriving Show
-
-instance Pretty (RE Char) where
-  pretty (Sym c)                 = PP.char c
-  pretty Epsilon                 = PP.char '_'
-  pretty (Concatenate Epsilon a) = pretty a
-  pretty (Concatenate a Epsilon) = pretty a
-  pretty (Concatenate a b)       = pretty a <> pretty b
-  pretty (Alternate a b)         = PP.parens $ pretty a <> PP.char '|' <> pretty b
-  pretty (Star (Sym c))          = PP.char c <> PP.char '*'
-  pretty (Star a)                = PP.parens (PP.pretty a) <> PP.char '*'
-  pretty (Looper i a)              = PP.brackets $ PP.int i <> PP.space <> PP.pretty a
-
 
 rex :: Dom () (RE a)
 rex = Dom
@@ -230,9 +217,10 @@ rex = Dom
   , identity    = const Epsilon
   , concatenate = const Concatenate
   , alternate   = const Alternate
-  , closure     = const Star
-  , iterate     = const Looper
-  } 
+  , closure     = const (\_ -> Star)
+  , iterate     = (\_ x e -> case x of {Just i -> Iterate i e; Nothing -> error "err"})
+  }
+
 
 ppcfg :: (Pretty e, Show v, Show e) => Cfg v e -> String
 ppcfg = unlines . map k
@@ -256,4 +244,44 @@ ex1 =
     d = edge (V 'C') (Sym 'd') (V 'B')
     c = edge (V 'B') (Sym 'c') (V 'C')
     e = edge (V 'C') (Sym 'e') (V 'F')
+
+--- * Pretty ---------------------------------------------------------------------------------------------------------
+
+render :: Pretty a => a -> String
+render = show . pretty
+
+prettyTrees [] = PP.empty
+prettyTrees ts = (PP.vcat $ zipWith (<>) (repeat $ PP.text "+ ") (map (PP.align . pretty) ts))
+
+instance (Pretty a, Pretty b) => Pretty (Top a b) where
+  pretty (Top t []) = pretty t
+  pretty (Top t ts) = pretty t PP.<$$> prettyTrees ts
+
+instance Pretty a => Pretty (Tree a) where
+  pretty (Tree t []) = pretty t
+  pretty (Tree t ts) = pretty t PP.<$> prettyTrees ts
+
+
+instance Pretty (RE Char) where
+  pretty (Sym c)                 = PP.char c
+  pretty Epsilon                 = PP.char '_'
+  pretty (Concatenate Epsilon a) = pretty a
+  pretty (Concatenate a Epsilon) = pretty a
+  pretty (Concatenate a b)       = pretty a <> pretty b
+  pretty (Alternate a b)         = PP.parens $ pretty a <> PP.char '|' <> pretty b
+  pretty (Star (Sym c))          = PP.char c <> PP.char '*'
+  pretty (Star a)                = PP.parens (PP.pretty a) <> PP.char '*'
+  pretty (Iterate i a)           = PP.brackets $ PP.int i <> PP.space <> PP.pretty a
+
+
+instance {-# Overlapping #-} (Pretty v, Pretty e) => Pretty [Edge v e] where
+  pretty = PP.vcat . map pretty
+
+instance (Pretty v, Pretty e) => Pretty (Edge v e) where
+  pretty Edge{..} = pretty src <> PP.string "\t ~> \t" <> pretty dst <> PP.string " \t " <> pretty att
+
+instance Pretty v => Pretty (V v) where
+  pretty (V v)   = pretty v
+  pretty (In v)  = PP.char '>' <> pretty v
+  pretty (Out v) = PP.char '<' <> pretty v
 
